947a502fcdc3c130c0cc4d77732e2ac57d42d0af
hubdogestor <sistemas@hubdogestor.com.br>
2025-10-28 17:58:41 -0300

    chore: update dependencies and add ESLint ignore file
    - Updated various dependencies in package.json to their latest versions.
    - Added a new .eslintignore file to exclude build artifacts and node_modules from linting.
    - Created a new .eslintraw.json file with an undefined value.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,10 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { ignores: ["**/node_modules/**", ".next/**", "dist/**", "build/**", ".turbo/**", "package-lock.json", "components.json", "**/*.css", ".eslintraw.json"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, settings: { react: { version: "detect" } } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], ...pluginReact.configs.flat.recommended, rules: { 'react/react-in-jsx-scope': 'off' } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },

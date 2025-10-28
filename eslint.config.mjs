e0219880176c31476c5d4267d6effee5c1c25dce
hubdogestor <sistemas@hubdogestor.com.br>
2025-10-28 18:10:16 -0300

    ok

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import nextPlugin from "eslint-config-next";
 
 export default defineConfig([
   { ignores: ["**/node_modules/**", ".next/**", "dist/**", "build/**", ".turbo/**", "package-lock.json", "components.json", "**/*.css", ".eslintraw.json"] },
@@ -17,4 +18,5 @@ export default defineConfig([
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  nextPlugin,
 ]);
